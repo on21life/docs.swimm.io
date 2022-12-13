db01de9ae0cb7b920b77bda5f3378a06ff625f09
Eden Hermelin <eden@swimm.io>
2022-12-13 11:38:57 +0200

    chore(layout): update template

--- src/theme/Layout.js
+++ src/theme/Layout.js
@@ -12,8 +12,10 @@ export default function Layout(props) {
                         `default-src 'self';
                          style-src 'self' 'unsafe-inline';
                          img-src 'self' https://avatars.githubusercontent.com/ https://www.google-analytics.com/ https://github.com/ blob: data:;
-                         font-src 'self' data:; script-src 'self' 'unsafe-inline' https://cdn.segment.com/ https://static.hotjar.com/ https://www.googletagmanager.com/ https://www.google-analytics.com/;
-                         connect-src 'self' https://www.google-analytics.com/ https://o1000337.ingest.sentry.io/ https://cdn.segment.com/ https://api.segment.io/ https://stats.g.doubleclick.net/`
+                         font-src 'self' data:;
+                         frame-src 'self' https://*.hotjar.com/;
+                         script-src 'self' 'unsafe-inline' https://cdn.segment.com/ https://www.googletagmanager.com/ https://www.google-analytics.com/ https://*.hotjar.com/;
+                         connect-src 'self' https://www.google-analytics.com/ https://o1000337.ingest.sentry.io/ https://cdn.segment.com/ https://api.segment.io/ https://stats.g.doubleclick.net/ https://*.hotjar.com/ wss://*.hotjar.com`
                     }
                 />
             </Head>
