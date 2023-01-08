3e985e6aa834271743b0b9104312d554d5a5d0b1
Omer Rosenbaum <omer@swimm.io>
2023-01-08 18:50:23 +0200

    no /docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -161,7 +161,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             items: [
               {
                 label: 'Quickstart',
-                to: '/docs/getting-started-guide',
+                to: '/getting-started-guide',
               },
               {
                 label: 'Use Cases',
