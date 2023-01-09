8610277d411eb1375244b552cbf4608b795ee7e1
Omer Rosenbaum <omer@swimm.io>
2023-01-09 18:49:59 +0200

    Fix redirects

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         createRedirects(existingPath) {
           if (existingPath.startswith('/')) {
             return [
-              existingPath = '/docs' + existingPath
+              '/docs' + existingPath
             ];
           }
           return undefined; // Return a falsy value: no redirect created
